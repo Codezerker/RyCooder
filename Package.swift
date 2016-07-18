238de50df3fbe38cf9d6205288a5cbb3c83913ad
Yan Li <yan.li@ef.com>
2016-07-18 10:47:18 +0800

    Add FisherYates package as dependency

--- Package.swift
+++ Package.swift
@@ -1,6 +1,9 @@
 import PackageDescription
 
 let package = Package(
-  name: "RyCooder"
+  name: "RyCooder",
+  dependencies: [
+    .Package(url: "https://github.com/apple/example-package-fisheryates.git", majorVersion: 1),
+  ]
 )
 
